a6b2d385504a6676b4c83bdb54a6e46b2f987671
marcomorenilla <167623114+marcomorenilla@users.noreply.github.com>
2024-06-04 18:38:00 +0200

    cambiamos estructura
    Metemos index.php en carpeta views y subimos carpeta proyecto php para utilizar código

---
+++ public/tienda/includes/conexionbd.php
@@ -0,0 +1,24 @@
+<?php
+$host = "localhost"; 
+$username = "adminpi"; 
+$password = "Morcilla01."; 
+$dbname = "aaejemplopi"; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Establecer el modo de error de PDO a excepción
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar con la base de datos: " . $e->getMessage());
+}
+
+/*PDO significa PHP Data Objects. 
+Es una extensión de PHP que proporciona 
+una interfaz orientada a objetos para interactuar 
+con bases de datos relacionales. 
+PDO permite a los desarrolladores acceder y 
+manipular bases de datos de manera más segura y consistente, 
+independientemente del tipo de base de datos 
+que se esté utilizando.*/
+
+?>
