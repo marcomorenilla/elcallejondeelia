dfa8d82a2a14566b0438a659be74a7f55bfa2c30
marcomorenilla <167623114+marcomorenilla@users.noreply.github.com>
2024-06-14 15:37:46 +0200

    actualizo main
    no sé por qué se me vuelve una versión atrás

--- public/includes/home.php
+++ public/includes/home.php
@@ -4,8 +4,10 @@ include 'conexionbd.php';
 // prueba página php
 $query_inicio = $pdo->query("SELECT texto FROM textos WHERE id_texto = 12");
 $logo_tienda = $pdo->query("SELECT enlace FROM imagenes WHERE id_imagen = 11");
+$query_titulo = $pdo -> query("SELECT texto FROM textos WHERE id_texto=16");
 $inicio = $query_inicio->fetch(PDO::FETCH_ASSOC);
 $logo = $logo_tienda->fetch(PDO::FETCH_ASSOC);
+$titulo = $query_titulo ->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!-- Sección home -->
@@ -14,7 +16,7 @@ $logo = $logo_tienda->fetch(PDO::FETCH_ASSOC);
         <img src="<?php echo $logo['enlace']; ?>" alt="logo">
     </div>
     <div class="box-title">
-        <h1>El Callejón de Elia</h1>
+        <h1><?php echo $titulo['texto']?></h1>
         <p><?php echo $inicio['texto']; ?></p><br><br>
         <form>
             <label>Buscar: </label>
--- public/views/index.php
+++ public/views/index.php
@@ -104,7 +104,7 @@
 
     <!--Abro footer-->
     <footer>
-        <a href="../white.html" target="_blank">Avisos legales</a>
+        <a href="../legal.html" target="_blank">Avisos legales</a>
     </footer>
     <!--Cierro footer-->
 </body>
